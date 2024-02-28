7d098fe8b32c96c857844def89f2146ca59208cf
JAHNAVI D <JAHNAVI D@LAPTOP-FI3BSN04>
2024-02-28 16:30:51 +0530

    TL===Branch1==task2

---
+++ GitProj/src/main/java/com/nt/payment/PaymentOperations.java
@@ -0,0 +1,13 @@
+package com.nt.payment;
+
+public class PaymentOperations {
+	public String doCreditcardPayments() {
+		return "Credit card payment is done";
+	}
+	public String doDebitcardPayments() {
+		return "Debit card payment is done";
+	}
+	public String doUPIPayments() {
+		return "UPI payment is done";
+	}
+}
