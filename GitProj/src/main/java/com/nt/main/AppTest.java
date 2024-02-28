3627895afe32d6e2421f2e86bba236aad757f5f0
JAHNAVI D <JAHNAVI D@LAPTOP-FI3BSN04>
2024-02-28 16:57:05 +0530

    test commit

--- GitProj/src/main/java/com/nt/main/AppTest.java
+++ GitProj/src/main/java/com/nt/main/AppTest.java
@@ -1,13 +1,15 @@
 package com.nt.main;
 
-public class AppTest {
+import com.nt.service.Arithemeticoperations;
+
+public class AppTest extends Arithemeticoperations{
 	public static void main(String[] args) {
-		/**ArithmeticOperations ar = new ArithmeticOperations();
+		Arithemeticoperations ar = new Arithemeticoperations();
 		System.out.println("sum:"+ar.sum(100,200));
 		System.out.println("sub:" +ar.sub(100,200));
 		System.out.println("=====");
 		System.out.println("===");
-		System.out.println("mul:"+ar.mul(100,200));**/
+		//System.out.println("mul:"+ar.mul(100,200));
 	}
 
 }
