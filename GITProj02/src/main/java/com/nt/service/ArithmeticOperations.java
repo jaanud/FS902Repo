e53d6fa0cdaaee4bf88117f227cd9cdef52343cf
JAHNAVI D <JAHNAVI D@LAPTOP-FI3BSN04>
2024-02-28 13:51:53 +0530

    first comment

--- GITProj02/src/main/java/com/nt/service/ArithmeticOperations.java
+++ GITProj02/src/main/java/com/nt/service/ArithmeticOperations.java
@@ -10,7 +10,10 @@ public class ArithmeticOperations
 	public int mul(int x, int y)
 	{
 		return x*y;
-	}	
+	}
+	public int sub(int x, int y) {
+		return x-y;
+	}
 	public static void main( String[] args )
 	
     {
