c2db91e981ff67279fbd22288860fad010ad5633
JAHNAVI D <JAHNAVI D@LAPTOP-FI3BSN04>
2024-02-28 16:41:26 +0530

    team commit

--- GitProj/src/main/java/com/nt/main/AppTest.java
+++ GitProj/src/main/java/com/nt/main/AppTest.java
@@ -2,12 +2,12 @@ package com.nt.main;
 
 public class AppTest {
 	public static void main(String[] args) {
-		ArithmeticOperations ar = new ArithmeticOperations();
+		/**ArithmeticOperations ar = new ArithmeticOperations();
 		System.out.println("sum:"+ar.sum(100,200));
 		System.out.println("sub:" +ar.sub(100,200));
 		System.out.println("=====");
 		System.out.println("===");
-		System.out.println("mul:"+ar.mul(100,200));
+		System.out.println("mul:"+ar.mul(100,200));**/
 	}
 
 }
