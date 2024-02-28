19b9923ba265d458213335869d1140950cfd5b91
JAHNAVI D <JAHNAVI D@LAPTOP-FI3BSN04>
2024-02-28 13:12:47 +0530

    dev1 commit

---
+++ GITProj02/src/main/java/com/nt/service/ArithmeticOperations.java
@@ -0,0 +1,19 @@
+package com.nt.service;
+
+
+public class ArithmeticOperations 
+	
+{
+	public int sum(int x, int y) {
+		return x+y;
+	}
+	public int mul(int x, int y)
+	{
+		return x*y;
+	}	
+	public static void main( String[] args )
+	
+    {
+        System.out.println( "Hello World!" );
+    }
+}
